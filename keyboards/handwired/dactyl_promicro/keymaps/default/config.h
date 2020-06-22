608b0ae7684ada972f42624884d00f9c512dc59b
Douglas Chen <douglaspchen@gmail.com>
2020-06-21 19:37:25 -0700

    tweak layout, clean up

--- keyboards/handwired/dactyl_promicro/keymaps/default/config.h
+++ keyboards/handwired/dactyl_promicro/keymaps/default/config.h
@@ -16,10 +16,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
-
-#define USE_SERIAL
-
-#define MASTER_RIGHT
-// #define MASTER_RIGHT
-//#define EE_HANDS
-// Rows are doubled-up
